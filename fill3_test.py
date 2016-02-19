c1609c959318779a2858de345136ef1e8040abcd
Andrew Hamilton <and_hamilton@yahoo.com>
2016-02-19 09:20:33 +0000

    Coding style.

--- fill3_test.py
+++ fill3_test.py
@@ -6,72 +6,72 @@
 
 import unittest
 
-import fill3 as fill
+import fill3
 
 
 class WidgetTests(unittest.TestCase):
 
-    TEXT_A = fill.Text("A")
-    TEXT_B = fill.Text("B")
+    TEXT_A = fill3.Text("A")
+    TEXT_B = fill3.Text("B")
 
     def assert_string(self, appearance, expected_string):
-        self.assertEqual(str(fill.join("\n", appearance)), expected_string)
+        self.assertEqual(str(fill3.join("\n", appearance)), expected_string)
 
     def test_rows_widget(self):
-        rows = fill.Row([self.TEXT_A, self.TEXT_B])
+        rows = fill3.Row([self.TEXT_A, self.TEXT_B])
         self.assert_string(rows.appearance_min(), "AB")
-        rows = fill.Row([fill.Filler(self.TEXT_A),
-                         fill.Filler(self.TEXT_B)])
+        rows = fill3.Row([fill3.Filler(self.TEXT_A),
+                          fill3.Filler(self.TEXT_B)])
         self.assert_string(rows.appearance((4, 1)), "A B ")
 
     def test_columns_widget(self):
-        columns = fill.Column([self.TEXT_A, self.TEXT_B])
+        columns = fill3.Column([self.TEXT_A, self.TEXT_B])
         self.assert_string(columns.appearance_min(), "A\n"
                                                      "B")
 
     def test_text_widget(self):
         self.assert_string(self.TEXT_A.appearance_min(), "A")
         text = "foo\nbar"
-        self.assert_string(fill.Text(text).appearance_min(), "foo\n"
-                                                             "bar")
+        self.assert_string(fill3.Text(text).appearance_min(), "foo\n"
+                                                              "bar")
 
     def test_portal_widget(self):
-        row = fill.Row([fill.Text("foo"), fill.Text("bar")])
-        portal = fill.Portal(row, (1, 0))
+        row = fill3.Row([fill3.Text("foo"), fill3.Text("bar")])
+        portal = fill3.Portal(row, (1, 0))
         self.assert_string(portal.appearance((5, 1)), "oobar")
         portal.position = (0, 10)
         self.assert_string(portal.appearance((1, 1)), " ")
 
     def test_border_widget(self):
-        contents = fill.Filler(self.TEXT_A)
-        self.assert_string(fill.Border(contents).appearance((3, 3)), "┌─┐\n"
-                                                                     "│A│\n"
-                                                                     "└─┘")
-        for empty_contents in [fill.Filler(fill.Text("")), fill.Column([])]:
-            self.assert_string(fill.Border(empty_contents).appearance((2, 2)),
+        contents = fill3.Filler(self.TEXT_A)
+        self.assert_string(fill3.Border(contents).appearance((3, 3)), "┌─┐\n"
+                                                                      "│A│\n"
+                                                                      "└─┘")
+        for empty_contents in [fill3.Filler(fill3.Text("")), fill3.Column([])]:
+            self.assert_string(fill3.Border(empty_contents).appearance((2, 2)),
                                "┌┐\n"
                                "└┘")
-        self.assert_string(fill.Border(fill.Column([])).appearance_min(),
+        self.assert_string(fill3.Border(fill3.Column([])).appearance_min(),
                            "┌┐\n"
                            "└┘")
-        self.assert_string(fill.Border(empty_contents).appearance((3, 3)),
+        self.assert_string(fill3.Border(empty_contents).appearance((3, 3)),
                            "┌─┐\n"
                            "│ │\n"
                            "└─┘")
-        text = fill.Text("abcdef")
-        self.assert_string(fill.Border(text, title="AB").appearance((8, 3)),
+        text = fill3.Text("abcdef")
+        self.assert_string(fill3.Border(text, title="AB").appearance((8, 3)),
                            "┌─ AB ─┐\n"
                            "│abcdef│\n"
                            "└──────┘")
 
     def test_placeholder_widget(self):
-        placeholder = fill.Placeholder(self.TEXT_A)
+        placeholder = fill3.Placeholder(self.TEXT_A)
         self.assert_string(placeholder.appearance_min(), "A")
         placeholder.widget = self.TEXT_B
         self.assert_string(placeholder.appearance_min(), "B")
 
     def test_scroll_bar(self):
-        scroll_bar = fill.ScrollBar(is_horizontal=True, bar_char="#")
+        scroll_bar = fill3.ScrollBar(is_horizontal=True, bar_char="#")
         self.assertEqual(scroll_bar.interval, (0, 0))
         self.assert_string(scroll_bar.appearance((1, 1)), "#")
         scroll_bar.interval = (0, 0.5)
@@ -80,7 +80,7 @@ class WidgetTests(unittest.TestCase):
         self.assert_string(scroll_bar.appearance((2, 1)), "# ")
         scroll_bar.interval = (0.25, 0.75)
         self.assert_string(scroll_bar.appearance((4, 1)), " ## ")
-        scroll_bar = fill.ScrollBar(is_horizontal=False, bar_char="#")
+        scroll_bar = fill3.ScrollBar(is_horizontal=False, bar_char="#")
         self.assertEqual(scroll_bar.interval, (0, 0))
         self.assert_string(scroll_bar.appearance((1, 1)), "#")
         scroll_bar.interval = (0, 0.5)
@@ -96,19 +96,19 @@ class WidgetTests(unittest.TestCase):
                                                           " ")
 
     def test_table_widget(self):
-        table = fill.Table([])
+        table = fill3.Table([])
         self.assert_string(table.appearance_min(), "")
-        table = fill.Table([[self.TEXT_A]])
+        table = fill3.Table([[self.TEXT_A]])
         self.assert_string(table.appearance_min(), "A")
-        table = fill.Table([[self.TEXT_A, self.TEXT_B]])
+        table = fill3.Table([[self.TEXT_A, self.TEXT_B]])
         self.assert_string(table.appearance_min(), "AB")
-        table = fill.Table([[self.TEXT_A, self.TEXT_B],
+        table = fill3.Table([[self.TEXT_A, self.TEXT_B],
                             [self.TEXT_B, self.TEXT_A]])
         self.assert_string(table.appearance_min(), "AB\n"
                                                    "BA")
-        label_foo = fill.Text("FOO")
-        table = fill.Table([[label_foo, self.TEXT_B],
-                            [self.TEXT_B, self.TEXT_A]])
+        label_foo = fill3.Text("FOO")
+        table = fill3.Table([[label_foo, self.TEXT_B],
+                             [self.TEXT_B, self.TEXT_A]])
         self.assert_string(table.appearance_min(), "FOOB\n"
                                                    "B  A")
 
